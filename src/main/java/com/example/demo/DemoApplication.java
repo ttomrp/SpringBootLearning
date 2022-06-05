42d3b1017ce3a51d6c7b8f9b984c68174c886708
Tom Perez <ttomrp@gmail.com>
2022-06-05 09:38:16 -0500

    first test api

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,14 @@ package com.example.demo;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.time.LocalDate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
 
 @SpringBootApplication
 @RestController
@@ -18,9 +21,14 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public static List<String> hello() {
-		List<String> words = List.of("Hello", "World", "!");
-		return words;
+	public static List<Student> hello() {
+		return List.of(new Student(
+			101L,
+			"Tom",
+			20,
+			LocalDate.of(1995, 8, 10),
+			"tom@email.com"
+		));
 	}
 
 }
--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -18,4 +18,51 @@ public class Student {
         this.dob = dob;
         this.email = email;
     }
+
+    public Student(String name, int age, LocalDate dob, String email) {
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getDOB() {
+        return this.dob;
+    }
+
+    public void setDOB(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
