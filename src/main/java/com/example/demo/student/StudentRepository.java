1114adba5c2a162de172cf44c3257f0013943bf8
Tom Perez <ttomrp@gmail.com>
2022-06-06 10:35:57 -0500

    delete functionality added

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,6 +1,9 @@
 package com.example.demo.student;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -8,4 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long>{
     
+    // map method to sql query
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    Optional<Student> findStudentByEmail(String email);
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -8,8 +8,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
-    // this is our service layer of the application.  it handles all the business logic for the app.
-    // @Service is how we annotate that this is a service component/class
+	// this is our service layer of the application. it handles all the business
+	// logic for the app.
+	// @Service is how we annotate that this is a service component/class
 
 	private final StudentRepository studentrepository;
 
@@ -18,18 +19,27 @@ public class StudentService {
 		this.studentrepository = studentrepository;
 	}
 
-    public List<Student> getStudents() {
+	public List<Student> getStudents() {
 		return studentrepository.findAll();
 	}
 
 	public void addNewStudent(Student student) {
 		Optional<Student> studentByEmail = studentrepository.findStudentByEmail(student.getEmail());
 		if (studentByEmail.isPresent()) {
-			// if student with this email exists, then throw exception for there already 
-			// being an existing student.  Cannot add new student.
+			// if student with this email exists, then throw exception for there already
+			// being an existing student. Cannot add new student.
 			throw new IllegalStateException("email taken");
 		}
-		
+
 		studentrepository.save(student);
 	}
+
+	public void deleteStudent(Long id) {
+		if (!studentrepository.existsById(id)) {
+			throw new IllegalStateException("student with Id " + id + " does not exist");
+		}
+
+		studentrepository.deleteById(id);
+	}
+
 }
